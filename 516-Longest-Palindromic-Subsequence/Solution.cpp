bed79eb5edb621d4a6918b2104c1b022d7a4de10
hariomjaiswal12 <omjaiswal942@gmail.com>
2024-11-13 08:53:00 +0530

    Add 0072 Edit Distance

---
+++ 516-Longest-Palindromic-Subsequence/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+ int f(string &text1, string &text2, int i, int j, vector<vector<int>>&dp){
+        int ans = 0;
+        if(i >= text1.size()) return 0;
+
+        if(j >= text2.size()) return 0;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        if(text1[i] == text2[j]){
+             ans = 1+f(text1, text2, i+1, j+1,dp);
+        }
+
+        else{
+            ans = 0+max(f(text1, text2, i+1, j,dp), f(text1, text2, i, j+1,dp));
+        }
+        dp[i][j] =ans;
+        return dp[i][j];
+    }
+    int longestPalindromeSubseq(string s) {
+          int n1 =s.size();
+          string r =s;
+          reverse(r.begin(),r.end());
+    
+        vector<vector<int>>dp(n1+1,vector<int>(n1+1,-1));
+       return f(s,r, 0, 0,dp);
+    }
+};
