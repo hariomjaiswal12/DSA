2749b2892907bba264e967e10d6484cd2f9dd2da
hariomjaiswal12 <omjaiswal942@gmail.com>
2026-04-17 10:12:00 +0530

    Add 0268 Missing Number

---
+++ 0268-missing-number/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size(),sum1=0,sum2=0;
+
+        for(int i=0; i<=n; i++){
+            sum1 = sum1 + i;
+        }
+
+        for(int i=0; i<n; i++){
+            sum2 = sum2 + nums[i];
+        }
+
+        return sum1-sum2;
+    }
+};
